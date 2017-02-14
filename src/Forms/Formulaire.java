28ac4ce6aed84590260b7657851ef1e2d11485a6
Airlinei4 <samirs.kadis@gmail.com>
2017-02-14 16:48:53 +0100

    Amélioration formulaire

--- src/Forms/Formulaire.java
+++ src/Forms/Formulaire.java
@@ -6,13 +6,12 @@ import java.awt.event.ActionListener;
 import java.util.*;
 import java.util.Map.Entry;
 
-import javax.swing.BorderFactory;
-import javax.swing.Box;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JTextField;
-import javax.swing.border.Border;
 
 import SystemeGestion.Entite;
 import airportComponents.Porte;
@@ -22,37 +21,27 @@ public class Formulaire extends JFrame implements ActionListener{
 
 	private ArrayList <JLabel> lbl = new ArrayList<JLabel>();
 
+
 	//private ArrayList<JLabel> 
 	private ArrayList <JTextField> txtF = new ArrayList<JTextField>() ;
 	private ArrayList<Object> data;
 	private ArrayList<Component> listFields;
 	private ComboBoxAvion comboBoxAv;
-	private ArrayList<IAvion> listAvions;
-	private ArrayList<Porte> listPortes;
 	private ComboBoxDepart comboBoxPiste;
 	private int position = 30;
 
-	private JFrame frame ;
 	private Entry<String, String> fieldName;
-	private Border borderAvion = BorderFactory.createEmptyBorder(0,60,10,10);
-	private Box boxListeAvions = Box.createVerticalBox();
-	private JLabel lblListeAvions;
 
 	public Formulaire(Map<String, String> listChamps, ArrayList<Object> listeValeur, Entite objet){
+		super("Formulaire");
 		JButton btnFinish = new JButton("Enregistrer");
 		listFields = new ArrayList<Component>();
 		
-
-		frame= new JFrame("Formulaire");
 		getContentPane().setLayout(null);
-		this.setSize(500, 400);
+//		this.setSize(500, 400);
 		this.setLocationRelativeTo(null);
-		this.setResizable(false);
 		
-		lblListeAvions = new JLabel("Liste des types d'avion:");
-		boxListeAvions.add(lblListeAvions);
-		getContentPane().add(boxListeAvions);
-		
+
 		Set<Entry<String, String>> setList = listChamps.entrySet();
 		Iterator<Entry<String, String>> it = setList.iterator();
 		int indexValeurs = 0;
@@ -89,37 +78,14 @@ public class Formulaire extends JFrame implements ActionListener{
 			}
 			
 			else if (fieldName.getValue()== "List IAvion"){
-				listAvions = new ArrayList<IAvion>();
 				lbl.add(new JLabel(fieldName.getKey()));
 				lbl.get(lbl.size()-1).setBounds(10, position , 152, 20);
 				getContentPane().add(lbl.get(lbl.size()-1));
 
-				comboBoxAv = new ComboBoxAvion();
-				comboBoxAv.addActionListener(this);
-				comboBoxAv.setBounds(189, position, 86, 20);
-				getContentPane().add(comboBoxAv);
-				listFields.add(comboBoxAv);
-				position+=30;
-			
-				JButton btnAdd = new JButton("+");
-				btnAdd.setBounds(300, position, 45, 20);
-				btnAdd.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent arg0) {
-
-						
-						IAvion tFAvion1 = comboBoxAv.getSelectedItem();
-						if(!listAvions.contains(tFAvion1)){
-							listAvions.add(tFAvion1);
-							lbl.add(new JLabel(tFAvion1.getNom()));
-							lbl.get(lbl.size()-1).setBounds(10, position+50 , 152, 20);
-							getContentPane().add(lbl.get(lbl.size()-1));
-							
-							
-							position+=30;
-						}
-					}
-				});
-				getContentPane().add(btnAdd);
+				ChampListAvion comboListAvion = new ChampListAvion((ArrayList<IAvion>) listeValeur.get(indexValeurs));
+				comboListAvion.setLocation(189, position);
+				getContentPane().add(comboListAvion);
+				listFields.add(comboListAvion);
 			}
 			else if(fieldName.getValue()== "int"){
 				lbl.add(new JLabel(fieldName.getKey()));
@@ -133,27 +99,6 @@ public class Formulaire extends JFrame implements ActionListener{
 				listFields.add(champEntier);
 			}
 			
-			else if(fieldName.getValue() == "List Porte"){
-				listPortes = new ArrayList<Porte>();
-				
-				lbl.add(new JLabel(fieldName.getKey()));
-				lbl.get(lbl.size()-1).setBounds(10, position , 152, 20);
-				getContentPane().add(lbl.get(lbl.size()-1));
-				
-				JButton btnAdd = new JButton("+");
-				btnAdd.setBounds(300, position, 45, 20);
-				btnAdd.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent arg0) {
-						Porte porte = new Porte("Porte"+listPortes.size());
-						listPortes.add(porte);
-						//ajout au conteneur dans la fenêtre du terminal
-						position+=30;
-					}
-				});
-				getContentPane().add(btnAdd);
-				listFields.add(btnAdd);
-			}
-			
 			else if (fieldName.getValue() == "Date") {
 				lbl.add(new JLabel(fieldName.getKey()));
 				lbl.get(lbl.size()-1).setBounds(10, position , 152, 20);
@@ -161,7 +106,6 @@ public class Formulaire extends JFrame implements ActionListener{
 				
 				ChampDate champDate = new ChampDate();
 				champDate.setLocation(189, position);
-//				champDate.setBounds(189, position, 86, 20);
 				getContentPane().add(champDate);
 				listFields.add(champDate);
 			}
@@ -179,12 +123,11 @@ public class Formulaire extends JFrame implements ActionListener{
 				getContentPane().add(txtF.get(txtF.size()-1));
 				listFields.add(txtF.get(txtF.size()-1));
 			}
-			position+=listFields.get(listFields.size()-1).getHeight()*1.5;
+			position+=listFields.get(listFields.size()-1).getHeight()+30;
 			indexValeurs++;
 		
 		}
-		 
-
+		
 		btnFinish.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				getContentPane().setVisible(false);
@@ -194,19 +137,14 @@ public class Formulaire extends JFrame implements ActionListener{
 					if(comp instanceof ComboBoxDepart){
 						data.add(((ComboBoxDepart) comp).getSelectedItem());
 					}else if (comp instanceof ComboBoxAvion){
-						if(listAvions != null){
-							data.add(listAvions);
-						}else{
-							data.add(((ComboBoxAvion) comp).getSelectedItem());
-						}
+						data.add(((ComboBoxAvion) comp).getSelectedItem());
+					}else if (comp instanceof ChampListAvion) {
+						data.add(((ChampListAvion) comp).getListAvion());
 					}else if(comp instanceof ChampEntier){
 						data.add(((ChampEntier) comp).getNumber());
 					}else if (comp instanceof ChampDate) {
 						data.add(((ChampDate) comp).getDate());
-					}else if(comp instanceof JButton && listPortes != null){
-						data.add(listPortes);
-					}
-					else{
+					}else{
 						data.add(((JTextField) comp).getText());
 					}
 				}
@@ -218,32 +156,12 @@ public class Formulaire extends JFrame implements ActionListener{
 					e.printStackTrace();
 				}
 				dispose();
-				
-
-//				//Enregistrement iAvion selectionne
-//				String tFAvion = comboBoxAv.getSelectedItem().toString();
-//				System.out.println("avion selectionné "+tFAvion);
-//
-//				//Enregistrement type d'avion boolPiste dans : true -> Depart ...
-//				Boolean boolPiste;
-//				String tFPiste = comboBoxPiste.getSelectedItem().toString();
-//				System.out.println("Type de piste selectionné "+tFPiste);
-//				if(tFPiste == "Depart")
-//					boolPiste = true;
-//				else
-//					boolPiste = false;
-//
-//				//Enregistrement texte ecris
-//				String [] txt = new String [txtF.size()];
-//				for (int jj=0; jj< txtF.size(); jj++ ){
-//					txt[jj] = txtF.get(jj).getText();
-//					System.out.println("txt ecrit "+ txt[jj]);
-//				}
-//				
 			}
 		});
-		btnFinish.setBounds(350, 300, 120, 50);
+		btnFinish.setBounds(350, position, 120, 50);
 		getContentPane().add(btnFinish);
+		setSize(500, btnFinish.getY()+btnFinish.getHeight()+50);
+		this.setResizable(false);
 
 		this.setVisible(true);
 	}
